b977e9327a5d23cdf5c6934ca2dfd330e03ef117
Lance Goyke <lance@lancegoyke.com>
2024-10-28 20:03:27 -0500

    add a Document model with a file field on it

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    """Document admin."""
+
+    list_display = ("name", "file")
--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    """Document model."""
+
+    name = models.CharField(max_length=255, blank=True)
+    file = models.FileField(upload_to="documents")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """Save the document."""
+        if not self.name and self.file:
+            self.name = self.file.name
+        super().save(*args, **kwargs)
