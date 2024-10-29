8e4b4e3374f932685de144bf6ca3f630306be4ed
Lance Goyke <lance@lancegoyke.com>
2024-10-28 19:08:06 -0500

    add helper function get_env_variable

--- pymupdf_django/settings.py
+++ pymupdf_django/settings.py
@@ -10,7 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name: str) -> str:
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f"Set the {var_name} environment variable."
+        raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
